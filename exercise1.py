ac59cb780b8df0e2762b24fab0e386b0f6f11fd5
David <dav.vivanco@duocuc.cl>
2026-06-02 22:00:57 -0400

    init

---
+++ exercise1.py
@@ -0,0 +1,10 @@
+
+
+
+while True:
+    print("-----MENU")
+    print("1. Agregar producto")
+    print("2. Mostrar producto")
+    print("3. Buscar producto")
+    print("4. Producto mas caro")
+    print("5. Salir")
